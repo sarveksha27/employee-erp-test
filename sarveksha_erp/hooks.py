6ac8e9d71491d09992437faeda623477733e7a9b
itspb-ux <pavitra.boga2006@gmail.com>
2026-07-26 15:55:38 +0000

    Add company fixtures and letterhead fixtures

--- sarveksha_erp/hooks.py
+++ sarveksha_erp/hooks.py
@@ -29,12 +29,13 @@ app_license = "mit"
 # app_include_js = "/assets/sarveksha_erp/js/sarveksha_erp.js"
 
 fixtures = [
-    {"dt": "Company"},
-    {"dt": "Fiscal Year"},
-    {"dt": "Fiscal Year Company"},
-    {"dt": "Supplier Group"},
-    {"dt": "Supplier"},
-    {"dt": "Custom Field"}
+    "Company",
+    "Custom Field",
+    "Letter Head",
+    "Supplier",
+    "Supplier Group",
+    "Fiscal Year",
+    "Fiscal Year Company",
 ]
 
 # include js, css files in header of web template
